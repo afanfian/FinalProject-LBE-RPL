2e2ff393cc7be397e0395114deddfc2cabf4370b
afanfian <afanfian14@gmail.com>
2021-09-26 15:32:59 +0700

    Memberi Border Tombol

--- fplbe-app/src/App.js
+++ fplbe-app/src/App.js
@@ -1,26 +1,12 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-// BrowserRouter : Sebagai komponen yang akan membungkus aplikasi
-// Route : Didalam route ada komponen
-// Switch : Membungkus didalamnya ada route
-// Link : Link a herf
 import "./App.css";
 function App() {
   const [user, setUser] = useState([]);
-  //UseState = komponen yang berfungsi untuk menyimpan datanya
-  //UseState = user berfungsi untuk menyimpan data array
-  //setUser = berfungsi untuk mengubah user
   useEffect(() => {
-    //fungsi dipanggil ketika aplikasi mau jalan
     fetch("https://api.cryptonator.com/api/full/btc-usd")
-      //fetch = membikin request ke https
       .then((respons) => respons.json())
-      //terima respon dan responnya diubah respon menjadi json
-      //res.json  = res(bisa diganti apa saja)
       .then((data) => setUser(data));
-    //dia akan mengubah user yang awalnya array kosong dan diubah menjadi data
-    //setUser = data
-    //fetch("https://exchange.bitcoin/api/trades?since=5",{method:"get"});
   }, []);
   console.log(user);
   return (
@@ -29,13 +15,19 @@ function App() {
       <BrowserRouter>
         <ul>
           <li>
-            <Link to="/profile">Profil</Link>
+            <Link to="/profile" className="Button">
+              Profil
+            </Link>
           </li>
           <li>
-            <Link to="/trading">Trading</Link>
+            <Link to="/trading" className="Button">
+              Trading
+            </Link>
           </li>
           <li>
-            <Link to="/markets">Markets</Link>
+            <Link to="/markets" className="Button">
+              Markets
+            </Link>
           </li>
         </ul>
         <switch>
@@ -50,7 +42,7 @@ function App() {
               <h4>LinkedIn</h4>
               <p>Fian Awamiry Maulana</p>
             </div>
-            <br/>
+            <br />
             <div className="profil-2">
               <h4>Name</h4>
               <p>Muhammad Akmal Rishwanda</p>
@@ -91,6 +83,7 @@ function App() {
           </Route>
           <Route path="/markets">
             <div className="marketsmal">
+              <h3>Market</h3>
               {user &&
                 user.ticker &&
                 user.ticker.markets &&
@@ -117,10 +110,5 @@ function App() {
       </BrowserRouter>
     </div>
   );
-  //eror = karena akses belom punya apa2
-  //{user && user.ticker && user.ticker.base}
-  //{user.ticker.base}
-  //fungsi map kayak for
 }
 export default App;
-//  Temanku kira kira paham apa engga
